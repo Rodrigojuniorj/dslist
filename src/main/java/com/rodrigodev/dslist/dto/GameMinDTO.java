7b256b261a9c627946261319c57c3c8abb6bf3d7
Rodrigo Junior <rodrigo.franco@unifio.edu.br>
2023-05-10 12:29:21 -0300

    aula 2 completa

--- src/main/java/com/rodrigodev/dslist/dto/GameMinDTO.java
+++ src/main/java/com/rodrigodev/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.rodrigodev.dslist.dto;
 
 import com.rodrigodev.dslist.entities.Game;
+import com.rodrigodev.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -18,6 +19,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
