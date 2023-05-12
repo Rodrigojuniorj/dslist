89934351176cb538c4351b81de11cf115cc5ad48
Rodrigo Junior <rodrigo.franco@unifio.edu.br>
2023-05-12 12:40:08 -0300

    finalizando o projeto

--- src/main/java/com/rodrigodev/dslist/services/GameListService.java
+++ src/main/java/com/rodrigodev/dslist/services/GameListService.java
@@ -3,9 +3,12 @@ package com.rodrigodev.dslist.services;
 import com.rodrigodev.dslist.dto.GameListDTO;
 import com.rodrigodev.dslist.dto.GameMinDTO;
 import com.rodrigodev.dslist.entities.GameList;
+import com.rodrigodev.dslist.projections.GameMinProjection;
 import com.rodrigodev.dslist.repositories.GameListRepository;
+import com.rodrigodev.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,8 +18,27 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int souceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(souceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = souceIndex < destinationIndex ? souceIndex : destinationIndex;
+        int max = souceIndex < destinationIndex ? destinationIndex : souceIndex;
+
+        for (int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
